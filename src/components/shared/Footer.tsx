530a1d0cad1d903391c724e20c3361b13fdd8452
mazhars-git <mazharulislam611@gmail.com>
2025-02-10 08:06:37 +0600

    first commit

---
+++ src/components/shared/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FooterPage = () => {
+  return (
+    <div>
+      <h1>This is footer</h1>
+    </div>
+  );
+};
+
+export default FooterPage;
---
+++ src/components/shared/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavbarPage = () => {
+  return (
+    <div>
+      <h1>This is Navbar</h1>
+    </div>
+  );
+};
+
+export default NavbarPage;
